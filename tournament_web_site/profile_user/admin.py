1b9458a848d07e1dfa49be4a8eff3f3f6310f652
Kult_Bestia <danaiek2003@mail.ru>
2025-01-25 17:35:16 +0600

    created tournament app

--- tournament_web_site/profile_user/admin.py
+++ tournament_web_site/profile_user/admin.py
@@ -4,4 +4,4 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-	list_display = ['username', 'email', 'age', 'avatar']
+	list_display = ['username', 'email', 'age', 'avatar', 'team']
---
+++ tournament_web_site/tournaments/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from games.models import Game
+from django.contrib.auth import get_user_model
+
+
+class Tournament(models.Model):
+    CHOICES_TEAM_SIZE = (('1', 'solo'), ('2', 'duo'), ('3', 'trio'), ('4', 'squad'), ('5', 'FULL'))
+    game = models.ForeignKey(Game, verbose_name='Игра', on_delete=models.CASCADE)
+    registration_start_date = models.DateTimeField(verbose_name='Дата начала регистрации', auto_now_add=True)
+    start_date = models.DateTimeField(verbose_name='Дата начала турнира')
+    team_size = models.CharField(verbose_name='Размер команды', max_length=1, choices=CHOICES_TEAM_SIZE)
+    maximum_number_of_teams = models.PositiveIntegerField(verbose_name='Максимальное количество команд')
+    price = models.PositiveIntegerField(verbose_name='Цена взноса')
+
+    def __str__(self):
+        return f'{self.game} {self.get_team_size_display()} #{self.pk}'
+    
+    class Meta:
+        verbose_name = 'Турнир'
+        verbose_name_plural = 'Турниры'
+
+class Team(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=100)
+    logo = models.ImageField(upload_to='logo/', verbose_name='Логотип', blank=True, null=True)
+    commander = models.OneToOneField(get_user_model(), verbose_name='Командир', on_delete=models.CASCADE, related_name='commander')
+    tournament = models.ForeignKey(Tournament, verbose_name='Турнир', blank=True, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Meta:
+        verbose_name = 'Команда'
+        verbose_name_plural = 'Команды'
+
+class TeamRegistration(models.Model):
+    team = models.ForeignKey(Team, verbose_name='Команда', on_delete=models.CASCADE)
+    player = models.OneToOneField(get_user_model(), verbose_name='Игрок', on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name='Дата подачи заявки', auto_now_add=True)
+    
+    class Meta:
+        verbose_name = 'Заявка в команду'
+        verbose_name_plural = 'Заявки в команды'
+
+class TournamentRegistration(models.Model):
+    tournament = models.ForeignKey(Tournament, verbose_name='Турнир', on_delete=models.CASCADE)
+    team = models.OneToOneField(Team, verbose_name='Команда', on_delete=models.CASCADE)
+    date = models.DateTimeField(verbose_name='Дата подачи заявки', auto_now_add=True)
+    created_by = models.ForeignKey(get_user_model(), verbose_name='Модератор', on_delete=models.CASCADE)
+    status = models.CharField(verbose_name='Статус', max_length=20, choices=(('ожидание', '1'), ('ожидание оплаты', '2'), ('подтверждения оплаты', '3'), ('оплата подтверждена', '4')))
+    
+    class Meta:
+        verbose_name = 'Заявка на турнир'
+        verbose_name_plural = 'Заявки на турниры'
