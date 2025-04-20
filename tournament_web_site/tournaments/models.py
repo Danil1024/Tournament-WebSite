1080951117e54260d2eb13eae36a747cc1473e45
Kult_Bestia <danaiek2003@mail.ru>
2025-04-20 19:18:37 +0600

    added main apge

--- tournament_web_site/tournaments/models.py
+++ tournament_web_site/tournaments/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from games.models import Game
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from datetime import timedelta
 
 
 class Tournament(models.Model):
     CHOICES_TEAM_SIZE = (('1', 'solo'), ('2', 'duo'), ('3', 'trio'), ('4', 'squad'), ('5', 'FULL'))
     game = models.ForeignKey(Game, verbose_name='Игра', on_delete=models.CASCADE)
-    registration_start_date = models.DateTimeField(verbose_name='Дата начала регистрации', auto_now_add=True)
+    creation_date = models.DateTimeField(verbose_name='Дата создания турнира', auto_now_add=True)
+    registration_start_date = models.DateTimeField(verbose_name='Дата начала регистрации')
     start_date = models.DateTimeField(verbose_name='Дата начала турнира')
     team_size = models.CharField(verbose_name='Размер команды', max_length=1, choices=CHOICES_TEAM_SIZE)
     maximum_number_of_teams = models.PositiveIntegerField(verbose_name='Максимальное количество команд')
@@ -14,6 +17,19 @@ class Tournament(models.Model):
 
     def __str__(self):
         return f'{self.game} {self.get_team_size_display()} #{self.pk}'
+    
+    def clean(self):
+        super().clean()
+
+        if self.start_date < self.registration_start_date:
+            raise ValidationError({
+                'start_date': 'Дата начала турнира не может быть раньше даты начала регистрации.'
+            })
+
+        if self.start_date - self.registration_start_date < timedelta(days=1):
+            raise ValidationError({
+                'start_date': 'Между началом регистрации и началом турнира должен быть минимум 1 день.'
+            })
     
     class Meta:
         verbose_name = 'Турнир'
--- tournament_web_site/tournaments/views.py
+++ tournament_web_site/tournaments/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Tournament
 
-# Create your views here.
+class MainPage(ListView):
+    model = Tournament
+    template_name = "tournaments/main_page.html"
+    context_object_name = "tournaments"
+
+    def get_queryset(self, *args, **kwargs):
+        return Tournament.objects.order_by('-creation_date')[:4]
